a5d7d4a68605c2e377acb8d49209ec0142862a5b
Maria Zamora <Zamora.Mariac@gmail.com>
2015-05-27 18:18:44 -0500

    more

---
+++ index/tabs.js
@@ -0,0 +1,15 @@
+$(document).on('ready', start);
+
+$(section).on('click', function(e){
+	var $target = $(e.target);
+	var targetName = $target.attr('section');
+
+}
+
+
+// $(‘NAV’).ON(‘CLICK”,FUNTION(E){
+// 	VAR $TARGET = $(E.TARGET):
+// 	VAR TARGETname = $TARGET.ATTR(‘HREF’);
+// $('main section').hide();
+// $(targetname).show();
+// })
